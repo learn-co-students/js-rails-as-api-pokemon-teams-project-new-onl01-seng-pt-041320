ea905611bfcf6e2b0a6c1fdb3775196098a34b23
Andrew Alteri <andrewsalteri@gmail.com>
2020-11-04 17:45:43 -0600

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,6 +2,6 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
-document.addEventListener("DOMContentLoaded", () => {
+console.log("%c Welcome to pokèmon trainers!", "color: yellow; -webkit-text-stroke-width: 1px; -webkit-text-stroke-color: blue;  font-size: 50px;");
+const trainersContainer = document.querySelector('main');
 
-})
