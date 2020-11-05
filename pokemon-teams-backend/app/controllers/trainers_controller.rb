74e9737139636e37d185d579c42181ac794a64ad
Andrew Alteri <andrewsalteri@gmail.com>
2020-11-04 22:49:44 -0600

    Done.

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -2,6 +2,6 @@ class TrainersController < ApplicationController
 
     def index
         trainers = Trainer.all 
-        render json: trainers 
+        render json: trainers, include: [:pokemons]
     end
 end
