dea12ed9c01a6bc790093011b675d67f5b9da0ca
vedransehovic <vedran.sehovic@gmail.com>
2020-11-08 12:01:56 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,14 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+//get trainers and related pokemons
+function fetchTrainersAndPokemons() {
+    return fetch(TRAINERS_URL)
+    .then(resp => resp.json())
+    .then(data => {
+        const trainers = data;
+        return trainers;
+    });
+}
+
