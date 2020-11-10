3e4bb24b86c798b6909d2b072366888025544047
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-09 21:15:17 -0800

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,14 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+//Pokemon and Trainers
+
+pokemon_trainers = () => {
+    fetch(TRAINERS_URL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        console.log(json)
+    });
