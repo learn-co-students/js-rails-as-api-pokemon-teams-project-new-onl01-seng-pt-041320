55c3677901fdbaabf8d8653199202d72ddaaaa94
chica25 <patricia@patriciaco.com>
2020-12-10 02:04:21 -0500

    Done.

---
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -0,0 +1,12 @@
+class TrainersController < ApplicationController
+
+    def index
+        trainers = Trainer.all
+        render json: trainers, includes: [:pokemons]
+    end
+
+    def show
+        trainer = Trainer.find(params[:id])
+        render json: trainer, include: [:pokemons]
+    end
+end
